a8ed6913495f0aeccc2a4bdd8a324e5d315ebeb2
Danilo Lekovic <danilo@lekovic.ca>
2022-01-05 18:37:37 -0800

    Relocated like button

--- src/Components/Post.tsx
+++ src/Components/Post.tsx
@@ -35,6 +35,9 @@ export default class Post extends React.Component<PostProps, PostState> {
         <div className="max-w-lg rounded overflow-hidden shadow-lg">
           { this.isImage(this.props.image) ? <img className="w-full" src={this.props.image} alt="Mountain" /> : <iframe className="w-full" src={this.props.image}></iframe> }
           <div className="px-6 py-4">
+
+          <button className="inline-block text-sm emoji-deactivated like-post float-right"
+                  onClick={(e) => this.clickLikeButton(e)}>&#10084;&#65039;</button>
             <div className="font-bold text-xl mb-2">{this.props.title}</div>
             <p className="text-gray-500 pb-2 text-base">
               {this.props.date}
@@ -42,11 +45,6 @@ export default class Post extends React.Component<PostProps, PostState> {
             <p className="text-gray-700 text-base">
               {this.props.explanation}
             </p>
-          </div>
-          <div className="px-6 pt-4 pb-2">
-            <button className="inline-block rounded-full pr-3 py-1 text-sm mr-2 mb-2 emoji-deactivated like-post"
-                  onClick={(e) => this.clickLikeButton(e)}>&#10084;&#65039;</button>
-            <button className="inline-block rounded-full pr-3 py-1 text-sm mr-2 mb-2">&#128203;</button>
           </div>
         </div>
       </div>
